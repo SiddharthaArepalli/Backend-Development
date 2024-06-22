3d9828a84cc24fe220c83da81277ff130e8dff3d
SiddharthaArepalli <140429954+SiddharthaArepalli@users.noreply.github.com>
2024-06-22 23:51:23 +0530

    Update app.js

--- Auth-2/app.js
+++ Auth-2/app.js
@@ -11,8 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 app.use(cookieParser());
-
-
 app.get("/", function(req,res){
     res.render('index');
 })
